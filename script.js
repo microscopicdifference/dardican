522de64d8466eaf14c3d2b229dbbc49414560797
microscopicdifference <50268141+microscopicdifference@users.noreply.github.com>
2022-10-05 19:42:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function initMap() {
   new google.maps.Map(document.getElementById("map"), {
-    mapId: "8e0a97af9386fef",
+    mapId: "760b63c14d90d0ab",
     center: { lat: 48.85, lng: 2.35 },
     zoom: 12,
   });
